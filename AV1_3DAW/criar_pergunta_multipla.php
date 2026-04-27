ff633872eccdc2ee2551de19a33cff7e8f783060
Isaque da Silva Souto <132595040+IsaqueSouto@users.noreply.github.com>
2026-04-27 00:02:08 -0300

    Create criar_pergunta_multipla.php

---
+++ AV1_3DAW/criar_pergunta_multipla.php
@@ -0,0 +1,18 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $linha = $_POST["pergunta"] . ";" . $_POST["a"] . ";" . $_POST["b"] . ";" . $_POST["c"] . ";" . $_POST["d"] . ";" . strtoupper($_POST["correta"]) . "\n";
+    file_put_contents("perguntas_multiplas.txt", $linha, FILE_APPEND);
+    echo "Pergunta salva!";
+}
+?>
+<form method="POST">
+    Pergunta:<br><input name="pergunta" required><br><br>
+    A:<input name="a" required><br>
+    B:<input name="b" required><br>
+    C:<input name="c" required><br>
+    D:<input name="d" required><br><br>
+    Resposta correta (A/B/C/D):<br>
+    <input name="correta" maxlength="1" required><br><br>
+    <button>Salvar</button>
+</form>
+<a href="index.php">Voltar</a>
