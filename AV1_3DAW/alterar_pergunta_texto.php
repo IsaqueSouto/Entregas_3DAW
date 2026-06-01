678ac76b88ad6b632d0947ad8042680ce7c8e4f4
Isaque da Silva Souto <132595040+IsaqueSouto@users.noreply.github.com>
2026-06-01 00:25:57 -0300

    Refactor form submission to use Fetch API

--- AV1_3DAW/alterar_pergunta_texto.php
+++ AV1_3DAW/alterar_pergunta_texto.php
@@ -2,15 +2,40 @@
 if ($_POST) {
     $id = $_POST['id'];
     $nova = $_POST['pergunta'];
+
     $linhas = file('perguntas_texto.txt');
     $linhas[$id] = $nova . PHP_EOL;
+
     file_put_contents('perguntas_texto.txt', implode('', $linhas));
+
     echo "Pergunta alterada!";
+    exit;
 }
 ?>
-<form method="post">
+
+<form id="formAlterar">
     ID: <input name="id"><br>
     Nova pergunta: <input name="pergunta"><br>
     <button>Alterar</button>
 </form>
+
+<div id="mensagem"></div>
+
 <a href="index.php">Voltar</a>
+
+<script>
+document.getElementById("formAlterar").addEventListener("submit", function(e){
+
+    e.preventDefault();
+
+    fetch("alterar_pergunta_texto.php", {
+        method: "POST",
+        body: new FormData(this)
+    })
+    .then(r => r.text())
+    .then(t => {
+        document.getElementById("mensagem").innerHTML = t;
+    });
+
+});
+</script>
